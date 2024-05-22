8dde6ce9ee08feeb99a9af6fde0a98f4a35f9c6a
bayramkarahan <bayramk@gmail.com>
2024-05-22 08:36:46 +0300

    test

--- site/conf.py
+++ site/conf.py
@@ -15,7 +15,7 @@ smartquotes = False
 
 # -- Options for PDF output --
 
-pdf_documents = [('index', u'rst2pdf', u'5 Sınıf Ders Notları', u'BTY5'),]
+pdf_documents = [('index', u'rst2pdf', u'Bilişim Teknolojileri veYazılım Dersi', u'BTY 5. Sınıf'),]
 pdf_stylesheets = ['style-main.yaml', 'tango']
 pdf_font_path = ['/usr/share/fonts', '/usr/share/texmf-dist/fonts/']
 pdf_style_path = [ '.', 'site/_static']
